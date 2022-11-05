1d30ee8284fcd931290bca7915a92d41d83af4b5
agryz01 <agryz@yandex.ru>
2022-11-05 19:17:56 +0300

    PR14_10_3

--- controllers/users.js
+++ controllers/users.js
@@ -74,7 +74,12 @@ const login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'q@mDl|{rW|7K', { expiresIn: '7d' });
-      res.send({ token });
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+      })
+        .end();
+      // res.send({ token });
     })
     .catch(next);
 };
